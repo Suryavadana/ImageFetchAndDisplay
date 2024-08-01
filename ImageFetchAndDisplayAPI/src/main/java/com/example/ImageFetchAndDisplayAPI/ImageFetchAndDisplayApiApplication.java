4f0b4658b659bcab492f04151d4fd45cb8b7d31b
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-08-01 14:56:22 -0500

    Created API and UI

---
+++ ImageFetchAndDisplayAPI/src/main/java/com/example/ImageFetchAndDisplayAPI/ImageFetchAndDisplayApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImageFetchAndDisplayAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFetchAndDisplayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFetchAndDisplayApiApplication.class, args);
+	}
+
+}
---
+++ ImageFetchAndDisplayAPI/src/test/java/com/example/ImageFetchAndDisplayAPI/ImageFetchAndDisplayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImageFetchAndDisplayAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFetchAndDisplayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
